6f7b430c6448f18190c72af97f645a3128f2b81c
eric prostko <ericprostko9@gmail.com>
2016-12-08 16:18:29 -0500

    Added pseudocode to .rb

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,44 @@
+# Encrypt
+
+	# Make the Loop
+	
+		#-index set to 0, to start loop from the beginning of the word
+	
+		#-in order to encrypt, first give the method a name
+	
+		#-find the length with built-in method [.length]
+		
+		#-using length of the word given to us by .length the method will know how many times to loop through
+	
+		#-make index break point = to .length
+	
+	# Encrypt the password
+
+		#-add on .next to method_name so that every letter advances to next letter in the sequence of the alphabet
+	
+		#-use variable_password[index] connected with .next to complete the method
+	
+		#-close out loop
+
+
+# Decrypt
+
+	# Make the Loop
+	
+		#-put index set as "abc...xyz"
+
+		#-give method name: decrypt
+
+		#-find length of word with .length
+	
+		#-use length of the word to give condition to the loop how many times to cycle through password
+
+
+	# Decrypt the password
+
+		#-Revert the word to normal by making a loop that goes through the word using the index 
+
+		#-using the index[alphabet] find position of each letter in the password and subtact one from that index[position] once 
+	
+		#-take that new letter referenced by the index and insert it into the password index position 
+
