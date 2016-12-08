74bc26ee4376600182ff8cd30d48f5bc0bf94ba1
eric prostko <ericprostko9@gmail.com>
2016-12-08 18:23:01 -0500

    Added final UX input, and line-by-line code explanation

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -6,31 +6,36 @@
 def encrypt(password)
   
  index = 0
-  
+  #setting counter to zero
   encrypt_array = []
-
+  # making an empty array to put encrypted password inside
 	 while index < password.length
-	 
+	 #beginning while loop, setting break to length of input string
 	  if password[index] == "z" 
-	    
+	    #setting conditional logic to fix edge case problem with "z"
+	    # password[index] locates character index inside string, returns a letter
 	    encrypt_array << "a"
+	    # shoveling the a into the array
 	  else
-	   
+	   #else catching everything except "z"
 	    encrypt_array << password[index].next
-	    
+	    # password[index] returns a letter, then calling .next to return the next character in the sequence
+	    # shoveling encrypted character into array
     end
     
-
+    # Adding 1 to the counter, so loop will eventually end
   index += 1
   end
-  p encrypt_array
+  # Printing out results of encrypt
+  p "#{encrypt_array.join}"
 end
+
 
 	# Make the Loop
 
 		#-in order to encrypt, first give the method a name
 
-		#-index set to 0, to start loop from the beginning of the word
+		#-counter set to 0, to start loop from the beginning of the word
 	
 		#-find the length with built-in method [.length]
 		
@@ -46,28 +51,32 @@ end
 	
 		#-close out loop
 
+
 # Decrypt
 
 def decrypt(password)
-   
+   #naming the method and setting argument
 	index_counter = 0
-	
+	# beginning the counter and location of index inside string
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
-	
+	# This is what we will search inside for the letter index, then subtract 1 from, to return the letter prior
 	decrypt_array = []
+	# making empty array to shovel decrypted letters inside
 	
 	  until index_counter == password.length
+	  	# making an until loop break point equal to end of the length of the input string
 	
-		  encrypt_reverse = alphabet.index(password[index_counter])
-
-		  minused_letter = encrypt_reverse - 1
-		  
+		  alphabet_letter_index = alphabet.index(password[index_counter])
+		  	# finding indexed letter in password, then finding the location of that letter ( integer ) in alphabet, setting variable to encrypt_reverse
+		  minused_letter = alphabet_letter_index - 1
+		  	# subtracting 1 from index position ( integer ), giving decrypted letter index
 		  decrypt_array << alphabet[minused_letter]
-      
+      		# shoveling the returned letter from location in alphabet, (returns a letter)
 		  index_counter += 1
-      
+      		# adding one to the counter so the loop will eventually end
 	  end
-	  p decrypt_array
+	  p "#{decrypt_array.join}"
+	   # printing results
 end
 
 
