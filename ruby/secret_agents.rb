14dad427bf68c360fb39af5512374b75339de27d
eric prostko <ericprostko9@gmail.com>
2016-12-08 16:44:19 -0500

    Added encryption method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,17 +1,19 @@
 # Encrypt
-
+def encrypt(password)
 	# Make the Loop
-	
-		#-index set to 0, to start loop from the beginning of the word
-	
+
 		#-in order to encrypt, first give the method a name
-	
-		#-find the length with built-in method [.length]
+
+		#-index set to 0, to start loop from the beginning of the word
+	index = 0
 		
+	while index < password.length
+		#-find the length with built-in method [.length]
+		p password[index].next
 		#-using length of the word given to us by .length the method will know how many times to loop through
-	
+		index += 1
 		#-make index break point = to .length
-	
+	end
 	# Encrypt the password
 
 		#-add on .next to method_name so that every letter advances to next letter in the sequence of the alphabet
@@ -20,15 +22,19 @@
 	
 		#-close out loop
 
-
 # Decrypt
 
+def decrypt(password)
+	index_counter = 0
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
 	# Make the Loop
-	
+	while index_counter < password.length
 		#-put index set as "abc...xyz"
+		p password[index_counter] = alphabet.index[(password[index_counter].index)-1]
+															
 
-		#-give method name: decrypt
-
+		#-give method name: decrypt alphabet.index[]
+		index_counter += -1
 		#-find length of word with .length
 	
 		#-use length of the word to give condition to the loop how many times to cycle through password
