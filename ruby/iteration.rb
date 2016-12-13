052cdd59c3fd2320838a6d05d81499058e8f0988
eric prostko <ericprostko9@gmail.com>
2016-12-13 15:39:53 -0500

    Added pseudocode and release 2 hashes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,43 +6,157 @@
 	letters = ["aaa", "b", "c", "d", "e", "a", "b", "h"] 
 
 	roman_nums = {
-		"1" => "I",
-		'5'=> "V",
-		"10" => "X",
-		this: "C"
+		'I' => 1,
+		'V'=> 5,
+		"X" => 10,
+		"C" => 100
+	}
+
+	#running .delete on the array
+		puts "\n\nUsing .delete\n"
+		puts "This is before running the letters.delete"
+		p letters
+		puts "This is after running the letters.delete block"
+		p letters.delete("aaa")
+	
+
+	#running .delete on the hash
+		puts "This is after running the roman_nums.delete block"
+		p roman_nums
+		puts "This is after running the roman_nums.delete block"
+		roman_nums.delete("X")
+		p roman_nums
+
+	# Resetting the hash, after the deletion
+		roman_nums = {
+		'I' => 1,
+		'V'=> 5,
+		"X" => 10,
+		"C" => 100
 	}
 
 
 
-letters.delete("aaa")
-p letters
-
-new_number_array = []
-new_number_array = numbers.drop_while do |number|
-	number < 3
-end
-p new_number_array
 
 
-numbers.select! do |number|
-	p number > 2
-end
-p numbers
+	#running .drop_while on the array
+		puts "\n\nUsing .drop_while on array, and reject! on hash\n"
+		num_array = []
+		puts "This is before running the numbers.dropwhile block"
+		p numbers
+		num_array =  numbers.drop_while do |number|
+			number < 3
+		end
+		puts "This is after running the letters.drop_while block"
+		p num_array
 
-numbers.keep_if do |number|
-	number > 1
-end
-p numbers
+	# Running .reject! on hash
 
-numbers.delete_if do |number|
-	number < 3
-end
-p numbers
+		puts "This is before running the roman_nums.reject! block"
+		p roman_nums
+		puts "This is after running the roman_nums.reject! block"
+		roman_nums.reject! do |roman, num|
+			 num < 5
+		  end
+		 p roman_nums
 
 
 
+		 # Resetting the hash, after the deletion
+		roman_nums = {
+		'I' => 1,
+		'V'=> 5,
+		"X" => 10,
+		"C" => 100
+	}
 
 
+
+
+
+	# Running .select! on array
+		puts "\n\n  Using .select \n"
+		puts "This is before running the numbers.select! block"
+		p numbers
+		numbers.select! do |number|
+			number > 2
+		end
+		puts "This is after running the numbers.select! block"
+		p numbers
+		# Running .select! on hash
+		puts "This is before running the numbers.select! block"
+		p roman_nums
+		puts "This is after running the roman_nums.select! block"
+		roman_nums.select! do |roman, num|
+			roman > "I"
+		end
+		p roman_nums
+
+
+
+
+
+
+	 # Resetting the hash, after the deletion
+		roman_nums = {
+		'I' => 1,
+		'V'=> 5,
+		"X" => 10,
+		"C" => 100
+	}
+
+
+
+
+
+
+	# Running .keep_if on array
+	puts "Before number.keep_if"
+	p numbers
+	numbers.keep_if do |number|
+		number > 1
+	end
+	puts "After numbers.keep_if"
+	p numbers
+	# Running .keep_if on hash
+	puts "before roman_nums.keep_if"
+	p roman_nums
+	puts "After roman_nums.keep_if"
+	roman_nums.keep_if do |roman,num|
+		roman > "I"
+	end
+	
+	p roman_nums
+
+
+ # Resetting the hash, after the deletion
+		roman_nums = {
+		'I' => 1,
+		'V'=> 5,
+		"X" => 10,
+		"C" => 100
+	}
+
+
+
+
+	# Running .delete_if on hash
+	puts "before .delete_if"
+	numbers.delete_if do |number|
+		number < 3
+	end
+	puts "after .delete_if"
+	p numbers
+
+	# Running .delete_if on hash
+	puts "before .delete_if"
+	p roman_nums
+	roman_nums.delete_if do |roman, nums|
+		nums < 100
+	end
+	puts "after .delete_if"
+	p roman_nums
+	
 
 
 
@@ -61,6 +175,16 @@ p numbers
 # Running the .each method
  # Running .each on the array
  def each_map_exp
+
+ 	letters = ["aaa", "b", "c", "d", "e", "a", "b", "h"]
+
+	superheroes = {
+		superman: "flying",
+		batman: "World's greatest butler",
+		spiderman: "shooting web"
+	}
+
+
 	puts "\n\n Using .each\n"
 	#---------------------------------------
 	puts "This is before running the block."
@@ -95,11 +219,11 @@ p numbers
 
 	# Running .map on the hash and returning an array
 	puts "This is before running the superhero hash .map block"
-	p superhero
+	p superheroes
 	new_superhero_hash = superheroes.map do |superhero, power|
 		p "#{superhero}, #{power}"
 	end
-	puts "This is after running the superhero hash .map block"
+	puts "This is after running the superhero hash .map block\n"
 	p new_superhero_hash
 
 
@@ -113,16 +237,16 @@ p numbers
 		p letter.next
 	end
 	p "This is after running the letters array .map! block"
+	p letters
 
- # Running the destructive 
+ # Running the destructive .map! on the Hash --- NO BUENO
+ 	def using_map_on_hash 
 	p "This is before running the superhero hash .map! block"
 	p superheroes
 	superheroes.map! do |superhero, power|
 		p "#{superhero}, #{power}"
 	end
 	p "This is after running the superhero hash .map! block"
-
+	end
 end
 
-
-each_map_exp
