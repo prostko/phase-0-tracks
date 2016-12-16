154d07a9aac9f5b4988db7988caca07f71a42e49
eric prostko <ericprostko9@gmail.com>
2016-12-16 10:55:38 -0500

    Added pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,14 @@
+=begin
+	
+
+	
+Alias generator
+	Takes user name; first and last
+		get the name from the user
+
+	Creates a fake name
+		switches the first and last name
+		changing all the vowels to 
+
+	Returns the fake name
+=end
