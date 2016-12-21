8410743e33349d33dd8fbe02bb643d4c6b1f441a
eric prostko <ericprostko9@gmail.com>
2016-12-21 10:57:01 -0500

    Release 0

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,17 @@
 class Santa
+	def initialize
+		puts "Initializing Santa Instance ..."
+	end
 	def speak
-		
+		puts "Ho, Ho, Ho! Merry Christmas!"
+	end
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie} cookie!"
 	end
 end
+
+this = Santa.new
+that = Santa.new
+
+this.speak
+that.eat_milk_and_cookies("shortbread")
