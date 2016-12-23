0d5cf7dd3d68fa9e3d9b5557debcf61d978687f7
eric prostko <ericprostko9@gmail.com>
2016-12-23 12:11:32 -0500

    Added new shout module mixin, two new classes, and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,79 @@
+=begin
+#calling pre-existing module
+
+	require SecureRandom
+
+	SecureRandom.base64(50)
+
+	Math.sqrt(500)
+
+
 module Shout
-	# We'll put some methods here soon
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def self.yelling_happily(words)
+		words + "!!!" + ":D :D :D"
+	end
 end
+
+p Shout.yell_angrily("This is stupid")
+p Shout.yell_angrily("This is awesome")
+
+
+module Flight
+	def take_off(altitude)
+		puts "Taking off and ascending until reaching #{altitude}..."
+	end
+end
+
+class Bird
+	include Flight
+end
+
+class Plane
+	include Flight
+end
+
+
+
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
+=end
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		words + "!!!" + ":D :D :D"
+	end
+end
+
+class Ornithologist
+	include Shout
+end
+
+class Richard_Branson
+	include Shout
+end
+
+dude = Ornithologist.new
+rich = Richard_Branson.new
+
+p dude.yell_happily("A secretary bird")
+
+p rich.yell_angrily("If someone offers you an opportunity and you're not sure you can do it, say yes -- then learn how to do it later")
+
+
+
+
+
+
+
+
