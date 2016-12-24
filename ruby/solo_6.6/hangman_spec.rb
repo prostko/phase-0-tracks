31929c8a896f1ca1300e46bc71ce880870b730f1
eric prostko <ericprostko9@gmail.com>
2016-12-24 16:11:35 -0500

    Added testing for round count, end count, guess history

--- ruby/solo_6.6/hangman_spec.rb
+++ ruby/solo_6.6/hangman_spec.rb
@@ -11,4 +11,18 @@ describe Hangman do
 	it "reveals correct guess letter" do
 		expect(hangman.guess_letter("t")).to eq "t__t___"
 	end
+
+	it "sets round count equal to number of unique guesses made" do
+		hangman.guess_letter("t")
+		hangman.guess_letter("t")
+		hangman.guess_letter("e")
+		expect(hangman.round_count).to eq 2
+	end
+
+	it "keeps a history of guesses made by the user" do
+		hangman.guess_letter("t")
+		hangman.guess_letter("t")
+		hangman.guess_letter("e")
+		expect(hangman.guesses.uniq.join('')).to eq "te"
+	end
 end
