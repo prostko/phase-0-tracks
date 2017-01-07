07f2c4279aa7a41ae8d72b21f3c98fde64b6217d
eric prostko <ericprostko9@gmail.com>
2017-01-07 12:09:51 -0500

    added print report method

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,28 +1,37 @@
-# Virus Predictor
+ Virus Predictor
 
 # I worked on this challenge [by myself, with: ].
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+#Require_relative looks for the file in the directory that you are asking of it- looks in the directory 
+#Require- looks at very specific directories and where in that directory- looks in pre-set places that are related to Ruby
+#It allows two ruby files to be connected and for one file to pull information from the other
+#Loading the file at the top of the ruby file in order to use its information in the class/method, etc.
+#Require- is different because it pulls from a database instead of another ruby file- convenient subset
+#Where the program starts looking from different location
 require_relative 'state_data'
 
 class VirusPredictor
 
+  #initialize: allowing the user to set the instance variables to use throughout the class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #virus_effects: pulling data from state_data.rb and taking information to plug into the method (the final equation)
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
-  private
 
+  private #does method inside of the class but doesn't but can not access method outside of the class
+
+  #take information from state_data and predict number of deaths in those states based on
+  #population density, population in general of that state
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -41,6 +50,7 @@ class VirusPredictor
 
   end
 
+  # calculating the speed at which the population will become infected based on the states population density
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -68,7 +78,19 @@ end
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
+#pulling information from state_Data and plugging it into class VirusPredictor
 
+
+def show_all_states(hash)
+  hash.each do |key,state|
+   all_state = VirusPredictor.new(key, state[:population_density], state[:population])
+   p all_state.virus_effects
+  end
+end
+
+
+
+show_all_states(STATE_DATA)
 
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
