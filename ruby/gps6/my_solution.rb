ca334aee2b5a3c79d9c1ee6f6a7e0dcd0b70c869
eric prostko <ericprostko9@gmail.com>
2017-01-07 12:48:50 -0500

    Added reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,4 +1,4 @@
- Virus Predictor
+# Virus Predictor
 
 # I worked on this challenge [by myself, with: ].
 # We spent [#] hours on this challenge.
@@ -13,7 +13,7 @@
 require_relative 'state_data'
 
 class VirusPredictor
-
+attr_reader :state
   #initialize: allowing the user to set the instance variables to use throughout the class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
@@ -21,10 +21,12 @@ class VirusPredictor
     @population_density = population_density
   end
 
+
+
   #virus_effects: pulling data from state_data.rb and taking information to plug into the method (the final equation)
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
 
@@ -32,26 +34,31 @@ class VirusPredictor
 
   #take information from state_data and predict number of deaths in those states based on
   #population density, population in general of that state
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
+    
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      number_of_deaths = num_death_setter(0.4)
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      number_of_deaths = num_death_setter(0.3)
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      number_of_deaths = num_death_setter(0.2)
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      number_of_deaths = num_death_setter(0.1)
     else
-      number_of_deaths = (@population * 0.05).floor
+      number_of_deaths = num_death_setter(0.5)
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
+def num_death_setter(percentage)
+  (@population * percentage).floor
+end
+
   # calculating the speed at which the population will become infected based on the states population density
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
@@ -72,6 +79,11 @@ class VirusPredictor
 
   end
 
+=begin  def speed_setter(speed, gain)
+    speed += gain
+end
+=end  
+
 end
 
 #=======================================================================
@@ -84,14 +96,14 @@ end
 def show_all_states(hash)
   hash.each do |key,state|
    all_state = VirusPredictor.new(key, state[:population_density], state[:population])
-   p all_state.virus_effects
+   all_state.virus_effects
   end
 end
 
-
-
 show_all_states(STATE_DATA)
 
+
+=begin
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
 alabama.virus_effects
 
@@ -103,7 +115,25 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
-
+=end
 
 #=======================================================================
 # Reflection Section
+=begin
+-- One hash syntax uses hash rocket notation and also new lines between each key/value. 
+  the other uses colon notation, which makes use of syntactic sugar for symbols
+-- Require_relative begins its search for an appropirate file in the current directory, and can branch outward
+  Require begins its search in the app folder for gems and ruby libraries and things, and can possibly be changed
+-- You can iterate though a hash with each, map, any?, all?, keep_if, delete_if, each_key, each_value, a bunch of different methods will do it
+-- The variables are all class instance variables, so they can be used anywhere in the class. Some were passed that didn't need to be
+-- Require vs Require_relative, this is a difficult one to explain, and stackExchange didn't do a good job
+
+
+
+
+
+
+=end
+
+
+
