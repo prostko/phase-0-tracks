ef58c8f3049b259ad9fa652f5e16f9496496fdb7
eric prostko <ericprostko9@gmail.com>
2017-01-14 17:01:28 -0500

    Funtional creation of notes,clients_data,and collections

--- databases/crm/crm_database_module.rb
+++ databases/crm/crm_database_module.rb
@@ -34,10 +34,14 @@
 
 
 require 'sqlite3'
+require 'faker'
+#Faker::Config.locale = 'en-US'
+
 
 
 module CRM_database
-  def create_database
+
+  def create_database(db_clients)
 
   db_clients =  SQLite3::Database.new("clients.db")
 
@@ -45,14 +49,16 @@ module CRM_database
     CREATE TABLE IF NOT EXISTS collections (
     id INTEGER PRIMARY KEY,
     collection VARCHAR(255)
-    );
+    )
   SQL
 
   create_table_notes = <<-SQL
     CREATE TABLE IF NOT EXISTS notes (
       id INTEGER PRIMARY KEY,
-      note VARCHAR(1024)
-      );
+      notes VARCHAR(1024),
+      client_id INTEGER,
+      FOREIGN KEY (client_id) REFERENCES clients_data(id)
+      )
   SQL
 
   create_table_purchase_histories = <<-SQL
@@ -62,15 +68,17 @@ module CRM_database
     time VARCHAR(50),
     amount NUMERIC,
     collection_id INTEGER,
-    FOREIGN KEY (collection_id) REFERENCES collections(id)
-    );
+    client_id INTEGER,
+    FOREIGN KEY (collection_id) REFERENCES collections(id),
+    FOREIGN KEY (client_id) REFERENCES clients_data(id)
+    )
   SQL
 
 
-  create_table_basic_data = <<-SQL
-    CREATE TABLE IF NOT EXISTS basic_data (
+  create_table_clients_data = <<-SQL
+    CREATE TABLE IF NOT EXISTS clients_data (
       id INTEGER PRIMARY KEY,
-      name VARCHAR(255),
+      name VARCHAR(255) NOT NULL UNIQUE,
       nickname VARCHAR(255),
       gender VARCHAR(255),
       email VARCHAR(255),
@@ -80,23 +88,56 @@ module CRM_database
       shirt_size INTEGER,
       shoe_size INTEGER,
       birthday VARCHAR(50),
-      nationality VARCHAR(255),
-      purchase_history_id INTEGER,
-      note_id INTEGER,
-      FOREIGN KEY (purchase_history_id) REFERENCES purchase_histories(id),
-      FOREIGN KEY (note_id) REFERENCES notes(id)
-      );
+      nationality VARCHAR(255)
+      )
   SQL
 
-  # db_clients.execute(create_table_collections)
-  # db_clients.execute(create_table_notes)
-  # db_clients.execute(create_table_purchase_histories)
-  # db_clients.execute(create_table_basic_data)
+  db_clients.execute(create_table_collections)
+  db_clients.execute(create_table_notes)
+  db_clients.execute(create_table_purchase_histories)
+  db_clients.execute(create_table_clients_data)
+
+  
+# Creating methods to put into SQL, for data population
+  def create_clients(db,name,nickname,gendr,email,billing_address,shipping_address,phone_number,shirt_size,shoe_size,birthday,nationality)
+    db.execute("INSERT INTO clients_data(name,nickname,gender,email,billing_address,shipping_address,phone_number,shirt_size,shoe_size,birthday,nationality) VALUES (?,?,?,?,?,?,?,?,?,?,?)",[name,nickname,gendr,email,billing_address,shipping_address,phone_number,shirt_size,shoe_size,birthday,nationality]) 
+  end
+
+  def create_notes(db, client_id, note)
+    db.execute("INSERT INTO notes(notes,client_id) VALUES (?,?)", [note,client_id])
+  end
+
+
+  def create_collections_data # simple refs table
+    "INSERT INTO collections(collection) VALUES ('ready-to-wear'),('gone rogue'),('gone bleu'),('shoes'),('grenoble'),('longue saison'), ('accessories')"
+  end
+
+
+# creating iterating methods to use above mothods and populate the tables
+  def populate_clients_data(db_clients)
+      5.times do |x|
+        x % 2 == 0 ? gendr = 'male' : gender = 'female'
+        
+        create_clients(db_clients, Faker::Name.name,'none',gendr,Faker::Internet.email, Faker::Address.city+" "+Faker::Address.street_address+" "+Faker::Address.zip_code, Faker::Address.city+" "+Faker::Address.street_address+" "+Faker::Address.zip_code,Faker::PhoneNumber.cell_phone, Faker::Number.number(2), Faker::Number.digit, "#{Faker::Date.backward(9999)}", Faker::Address.country)
+      end
+    end
+
+  def populate_notes_data(db)
+
+    5.times do |x|
+      create_notes(db,Faker::Number.between(1,25), Faker::ChuckNorris.fact)
+    end
+  end
+
+  populate_clients_data(db_clients)
+  populate_notes_data(db_clients)
+  db_clients.execute(create_collections_data)
+
+ 
+ # implicit return the database
+  db_clients
   end
 end
-
-
-
 
 
 
