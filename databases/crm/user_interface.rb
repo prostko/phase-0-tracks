e99008d4b45d57f0c56ec23e76b7a58a652d0490
eric prostko <ericprostko9@gmail.com>
2017-01-14 11:15:04 -0500

    Initial Commit

---
+++ databases/crm/user_interface.rb
@@ -0,0 +1,20 @@
+require_relative 'CRM_database_module'
+
+module CRM_user_interface_module
+
+  def create
+    
+  end
+
+  def read
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+end
