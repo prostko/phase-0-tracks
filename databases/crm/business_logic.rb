689a6606fa34f73cf14c3d20a2ef64ff9ebf159f
eric prostko <ericprostko9@gmail.com>
2017-01-14 20:23:11 -0500

    Cleaned up and added delete, find_id, delete methods

--- databases/crm/business_logic.rb
+++ databases/crm/business_logic.rb
@@ -13,73 +13,37 @@ class BusinessLogic
     @db_clients = self.create_database(@db_clients)
   end
 
-  def read(table,column)
+  def read(table, column)
     db_clients.execute("SELECT #{table}.id, #{column} FROM #{table}").each do |row| puts "ID: #{row}" end
   end
 
-  def create
-    # input the user input into the database
-    # make sure name doesn't already exist
+  def read_all(table, name)
+    db_clients.execute("SELECT * FROM #{table} WHERE name = '#{name}'").each do |row| row.each do |item| puts "#{item}"end end
+  end
 
-    puts "What is the clients name? (First, Last)"
-    client_name = gets.chomp
-    puts "Nickname"
-    nickname = gets.chomp
-    puts "gender (male/female)"
-    gender = gets.chomp
-    puts "Birthday"
-    birthday = gets.chomp
-    puts "Nationality"
-    nationality = gets.chomp
-    puts "email"
-    email = gets.chomp
-    puts "Billing address"
-    billing = gets.chomp
-    puts "Shipping Address"
-    shipping = gets.chomp
-    puts "Phone number"
-    phone_number = gets.chomp
-    puts "Shirt size"
-    shirt_size = gets.chomp
-    puts "Jacket size"
-    jacket_size = gets.chomp
-    puts "Shoe size"
-    shoe_size = gets.chomp
-
+  def create(client_name, nickname, gender, email, billing, shipping, phone_number, shirt_size, jacket_size, shoe_size, birthday, nationality)
     db_clients.execute("INSERT INTO clients_data(name,nickname,gender,email,billing_address,shipping_address,phone_number,shirt_size,jacket_size,shoe_size,birthday,nationality)VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",[client_name,nickname,gender,email,billing,shipping,phone_number,shirt_size,jacket_size,shoe_size,birthday,nationality])
   end
 
-  def update
-    puts "Which table's information would you like to update?"
-    table = gets.chomp
-    case table
-    when "clients_data"
-      puts "Please input the name of the person to be updated."
-      table = gets.chomp
-      puts 
-    when "notes"
-    else raise ArgumentError "Input not valid"
-    end
-    update_command = <<-SQL
-
-    SQL
-    db_clients.execute()
+  def create_note(notes, client_id)
+    db_clients.execute("INSERT INTO notes(notes,client_id) VALUES (?,?)", [notes, client_id])
   end
 
-  def update(table, column, old_value, new_value)
+  def update(table, column, client, new_value)
+    # update_command = "UPDATE #{table} SET #{column} = '#{new_value}' WHERE name='#{client}'"
+    db_clients.execute("UPDATE #{table} SET #{column} = '#{new_value}' WHERE name='#{client}'")
+  end
 
-    update_command = "UPDATE #{table} SET #{column} = '#{new_value}' WHERE #{column}='#{old_value}'"
-    db_clients.execute(update_command)
+  def delete(table, name, id, client_id)
+     db_clients.execute("DELETE FROM #{table} WHERE #{id} = '#{client_id}'")
+   end 
 
+  def find_id(name)
+    db_clients.execute("SELECT id FROM clients_data WHERE name = '#{client}'").join.to_i
   end
 end
 
-
-
-this = BusinessLogic.new('db_clients')
-#this.read('clients_data','name')
-
-this.update("clients_data", "name", "ZZZ", "YYY")
-
-
-
+#  db_clients = BusinessLogic.new('db_clients')
+# # db_clients.update("clients_data", "name", "YYY", "ZZZ")
+# # client_id = db_clients.execute("SELECT id FROM clients_data WHERE name = 'ZZZ'")
+# db_clients.throwaway
